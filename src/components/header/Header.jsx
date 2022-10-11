0c90f407ec710afa26ecc4e46943e0c6c2dbf2fb
AnylPascal <a.tyon@icloud.com>
2022-10-11 01:55:19 -0500

    media query added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className= "container header__container">
         <h5>Hello I'm</h5>
         <h1>Anyl Pascal</h1>
-        <h5 className="text-light">Fullstack Development </h5>
+        <h5 className="text-light">Fullstack Developer </h5>
         <CTA />
         <HeaderSocials />
 

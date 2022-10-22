7cf417f02d2ad3e2df0e6f811d2745204457942d
AnylPascal <a.tyon@icloud.com>
2022-10-22 15:16:57 -0500

    github links corrected

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,7 +9,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/anylpascal/" target="_blank" > <SiLinkedin /></a>
-        <a href="https://github.com" target="_blank"><ImGithub /></a>
+        <a href="https://github.com/AnylPascal" target="_blank"><ImGithub /></a>
         <a href="https://medium.com/@AnylPascal" target="_blank"> <SiMedium /></a>
         <a href="https://stackoverflow.com/users/13615914/anyl-pascal.com" target="_blank"><ImStackoverflow /></a>
         <a href="https://www.upwork.com/freelancers/~0183a4dbf5bce03e3d?viewMode=1" target="_blank"><SiUpwork /></a>  

8d2f86838af4b83d01464b184cf36b70e036b7f7
AnylPascal <a.tyon@icloud.com>
2022-10-25 15:28:20 -0500

    updates

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,21 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Works</h5>
+        <h2>Portfolio</h2>
+      
+
+      <div className=" container portfolio__container">
+        <article className='portfolio__item'> 
+        <div className='portfolio__item-item'></div>
+        <h3>This is a portfolio item title</h3>
+        <a href="https://github.com" className='btn' target='_blank'></a>
+        <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        </div>
+
+    </section>
   )
 }
 
